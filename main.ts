12a2db0dbae2fddbb76c93845f999dfdf855e7af
Ekaterina <90045388+livicatarri@users.noreply.github.com>
2022-03-10 21:31:44 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function NumMap<Number>(n): object{/*создать new Map<number, null>() проитерироваться циклом от 0 до 100 и задать как ключ мапы, индекс, а как значение null.*/
+function NumMap<Number>(n){/*создать new Map<number, null>() проитерироваться циклом от 0 до 100 и задать как ключ мапы, индекс, а как значение null.*/
 let numNull =  new Map<Number, null>();
 for(let i = 1; i <n+1 ; i++){
         numNull.set(i,  null)
@@ -7,17 +7,21 @@ for(let i = 1; i <n+1 ; i++){
 }
 
 const numIf= NumMap(100);
+//console.log(numIf);
 const temp=numIf.keys()
-function mapConsole(keyConsole: temp){/**/
+function mapConsole<Array>(keyConsole: temp){/**/
 for (item of keyConsole){
 if ((item%3==0)&&(item%5!=0)){
-console.log("key:", item, "Devided by tree")}
+console.log("Devided by tree")}
 if ((item%5==0)&&(item%3!=0)){
-console.log("key:", item, "Oops, here is 5")}
+console.log("Oops, here is 5")}
 if ((item%5==0)&&(item%3==0)){
-console.log("key:", item, "I can believe in that!")}}
-    return 
+console.log("I can believe in that!")}
+else {
+    console.log("key:", item)
 }
-        
+}
+
+} 
 let temp1=mapConsole(temp)
 console.log(temp1)
