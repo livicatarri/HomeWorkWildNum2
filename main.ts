65dac9472ebb360707c962b890bbe0cf6859db24
Ekaterina <90045388+livicatarri@users.noreply.github.com>
2022-03-10 18:35:26 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
-
 function NumMap<Number>(n): Object{/*создать new Map<number, null>() проитерироваться циклом от 0 до 100 и задать как ключ мапы, индекс, а как значение null.*/
 let numNull =  new Map<Number, null>();
-for(let i = 1; i <= n ; i++){
+for(let i = 1; i <n+1 ; i++){
         numNull.set(i,  null)
 }
    return numNull
 }
-console.log(NumMap(100))
+//console.log(NumMap(100))
+
+
+
+
+let numIf= NumMap(100);/*2 пункт*/
+let temp=[]
+temp=numIf.keys()
+for ( item of temp){
+//console.log(item)
+if (item%3==0)
+console.log(item, "Devided by tree")
+            }
+        //console.log(temp)
+        
