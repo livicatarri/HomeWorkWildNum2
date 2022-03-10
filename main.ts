d3efcc4303e98cb53e85899c040ec28056126f7c
Ekaterina <90045388+livicatarri@users.noreply.github.com>
2022-03-11 00:49:51 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function NumMap<Number>(n){/*создать new Map<number, null>() проитерироваться циклом от 0 до 100 и задать как ключ мапы, индекс, а как значение null.*/
 let numNull =  new Map<Number, null>();
-for(let i = 1; i <n+1 ; i++){
+for(let i = 0; i < n ; i++){
         numNull.set(i,  null)
 }
    return numNull
@@ -18,34 +18,23 @@ console.log("Oops, here is 5")}
 if ((item%5==0)&&(item%3==0)){
 console.log("I can believe in that!")}
 else {
-    console.log("key:", item)
-}
-}
-
-}
-
+    console.log("key:", item)}}}
         
-let temp1=mapConsole(temp)
-console.log(temp1)
+mapConsole(temp)
 
-/*let arr= [["36", "не приватизирован", "4 собственника", "Парковая"],
+
+const Data:string[][] = [["36", "не приватизирован", "4 собственника", "Парковая"],
 ["45", "приватизирован", "3 собственника", "Луговая"],
 ["56", "не приватизирован", "1 собственник", "Луговая"],
 ["56", "приватизирован", "5 собственников", "Ленина"],
-["25", "не приватизирован", "2 собственника", "Ленина"]]*/
-
-interface Realtor{
-    square: number
-    privateHome: string;
-    countOwner: string;
-    location: string;
+["25", "не приватизирован", "2 собственника", "Ленина"]]
+function SearchData(data: string[][]){
+for (item of data) {
+    let square = Number(item[0])
+    let street = item[3]
+    console.log(((square <= 40 && street=== "Ленина") || (square=== 56)) ? true :  false)
 }
-const Arr : Realtor[] = [
-    {square:"36", privateHome:"не приватизирован", countOwner:"4 собственника", location:"Парковая"},
-    {square:"45", privateHome:"приватизирован", countOwner:"3 собственника", location:"Луговая"},
-    {square:"56", privateHome:"не приватизирован", countOwner:"1 собственник", location:"Луговая"},
-    {square:"56", privateHome:"приватизирован", countOwner:"5 собственников", location:"Ленина"},
-    {square:"25", privateHome:"не приватизирован", countOwner:"2 собственника", location:"Ленина"},
-    ]
-    console.log(Arr)
+}
+
+SearchData(Data)
     
