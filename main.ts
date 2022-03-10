e9ba1bea109e41590ffff1ef62129535b4a3a9bf
Ekaterina <90045388+livicatarri@users.noreply.github.com>
2022-03-10 17:30:22 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 
 function NumMap<Number>(n): Object{/*создать new Map<number, null>() проитерироваться циклом от 0 до 100 и задать как ключ мапы, индекс, а как значение null.*/
-let map1 =  new Map<Number, null>();
+let numNull =  new Map<Number, null>();
 for(let i = 1; i <= n ; i++){
-        map1.set(i,  null)
+        numNull.set(i,  null)
 }
-   return map1
+   return numNull
 }
 console.log(NumMap(100))
