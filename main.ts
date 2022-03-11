39f494e23f404cf9bb594d9b40249200903b6e66
Ekaterina <90045388+livicatarri@users.noreply.github.com>
2022-03-11 11:34:38 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function NumMapFill(n){  
+function NumMapFill(n: number): Map<number, null>{  
     let numNull =  new Map();
 for(let i = 0; i <  n ; i++){
         numNull.set(i,  null)
@@ -8,8 +8,8 @@ for(let i = 0; i <  n ; i++){
 
 console.log(NumMapFill(100));
 
-function mapConsoleMessage(keyConsole){/**/
-for (const item of keyConsole){
+NumMapFill(100).forEach(( value: null, item: number)=>{
+    //console.log(value);
 if ((item%3==0)&&(item%5!=0)){
 console.log("Devided by tree")}
 if ((item%5==0)&&(item%3!=0)){
@@ -17,10 +17,8 @@ console.log("Oops, here is 5")}
 if ((item%5==0)&&(item%3==0)){
 console.log("I can believe in that!")}
 else {
-    console.log("key:", item)}}}
-  //console.log(numIf);  
-//mapConsole(temp)
-mapConsoleMessage(NumMapFill(100).keys())
+    console.log("key:", item)}}
+    )
 
 let DataArr: string[][] = [["36", "не приватизирован", "4 собственника", "Парковая"],
 ["45", "приватизирован", "3 собственника", "Луговая"],
