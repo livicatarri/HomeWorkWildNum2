d99c48479e897304ba583e6882b49cc873110f77
Ekaterina <90045388+livicatarri@users.noreply.github.com>
2022-03-11 09:26:59 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
-function NumMap(n: number){/*создать new Map<number, null>() проитерироваться циклом от 0 до 100 и задать как ключ мапы, индекс, а как значение null.*/
+function NumMapFill(n: Number):Map<Number, null>  {/*создать new Map<number, null>() проитерироваться циклом от 0 до 100 и задать как ключ мапы, индекс, а как значение null.*/
 let numNull =  new Map();
-for(let i = 0; i < n ; i++){
+for(let i = 0; i <  n ; i++){
         numNull.set(i,  null)
 }
    return numNull
 }
 
-const numIf= NumMap(100);
-//console.log(numIf);
-const temp=numIf.keys()
-function mapConsole(keyConsole: number[]){/**/
-for (item of keyConsole){
+console.log(NumMapFill(100));
+
+function mapConsoleMessage(keyConsole: number[]): void{/**/
+for (const item of keyConsole){
 if ((item%3==0)&&(item%5!=0)){
 console.log("Devided by tree")}
 if ((item%5==0)&&(item%3!=0)){
@@ -19,21 +18,22 @@ if ((item%5==0)&&(item%3==0)){
 console.log("I can believe in that!")}
 else {
     console.log("key:", item)}}}
-        
-mapConsole(temp)
+  //console.log(numIf);  
+//mapConsole(temp)
+mapConsoleMessage(NumMapFill(100).keys())
 
-
-const Data:string[][] = [["36", "не приватизирован", "4 собственника", "Парковая"],
+const DataArr:string[][] = [["36", "не приватизирован", "4 собственника", "Парковая"],
 ["45", "приватизирован", "3 собственника", "Луговая"],
 ["56", "не приватизирован", "1 собственник", "Луговая"],
 ["56", "приватизирован", "5 собственников", "Ленина"],
 ["25", "не приватизирован", "2 собственника", "Ленина"]]
-function SearchData(data: string[][]): boolean{
-for (item of data) {
+function SearchDataApartament(dataSearch: string[][]): void{
+for ( const item of dataSearch) {
     let [square, , ,location] = item
-    //console.log(square)
-    console.log(((Number(square) <= 40 && location == "Ленина") || (Number(square)== 56)) ? true :  false)
-}
+      //return ((Number(square) <= 40 && location == "Ленина") || (Number(square)== 56)) ? true :  false
+    console.log (((Number(square) <= 40 && location === "Ленина") || (Number(square)== 56)) ? true :  false)
+   // return rezult
 }
 
-SearchData(Data)
+}
+SearchDataApartament(DataArr)
